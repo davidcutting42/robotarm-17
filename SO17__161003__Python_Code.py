a27b3de032aabfe3371fd6d19db68cf7d0ba710a
David Cutting <davecutting2000@gmail.com>
2016-10-07 00:53:54 +0000

    Added further functionality

--- SO17__161003__Python_Code.py
+++ SO17__161003__Python_Code.py
@@ -6,29 +6,46 @@ spnav_open()
 import Arm
 
 #Cartesian Space to Joint Space Libraries
-import math
+from math import *
 import numpy as np
 
-#Arduino Serial Communication
-import serial
-ser = serial.Serial('/dev/ttyACM0', 115200)
+#Arduino Communication
+import minimalmodbus
+arduino = minimalmodbus.Instrument('/dev/ttyACM0', 1) # port name, slave address (in decimal)
+#arduino.debug = True
 
-x = 0
-y = 0
-z = 0
-a = 0
-b = 0
-c = 0
-angles = [math.pi/4, math.pi/4, 0]
+#Target variables for Arm3Link functions
+targety = 0
+targetz = 0
+
+#Mouse scaling constant
+mouse_scal = 300
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#Minimum and maximum boundaries in mm
+maxy = 0
+maxz = 0
+miny = 0
+minz = 0
 
 #=================================================================
 
 #3D Mouse Input loop
 try:
     while True:
+        #Read Potentiometer Values from Arduino
+        Shoulder_Pot_Angle = arduino.read_register(2, 1)
+        Elbow_Pot_Angle = arduino.read_register(3, 1)
+        Shoulder_Pot_Angle_Raw = arduino.read_register(4, 0)
+        Elbow_Pot_Angle_Raw = arduino.read_register(5, 0)
+        #print "{}, {}, {}, {}".format(Shoulder_Pot_Angle_Raw, Elbow_Pot_Angle_Raw, Shoulder_Pot_Angle, Elbow_Pot_Angle)
+        #print Elbow_Pot_Angle_Raw
+        #print "regs read"
+        #Determine if there is change in the mouse
         event = spnav_poll_event()
         if (event != None):
             if (event.ev_type == SPNAV_EVENT_MOTION):
+                #Aquiring and parsing out translation and rotation variables from the mouse
                 tran = event.translation
                 rot = event.rotation
                 x = tran[0]
@@ -37,15 +54,21 @@ try:
                 a = rot[0]
                 b = rot[1]
                 c = rot[2]
-                yrad = y*180/np.pi
-                zrad = z*180/np.pi
-                angles = Arm.Arm3Link(q = [angles[0], angles[1], 0],L = np.array([255,255,1])).inv_kin(xy = (y, z))
-                shoulder = ((int(angles[0]*180/math.pi) * 10**3) / 10.0**3)
-                elbow = ((int(angles[1]*180/math.pi) * 10**3) / 10.0**3)
-            transmission = [shoulder, elbow]
-            transstring = str(transmission).strip('[]')
-            print transstring
-            ser.write(transstring)
+                #Scale mouse input to correct size
+                targety += (y/mouse_scal)
+                targetz += (z/mouse_scal)
+                print "{}, {}".format(targety, targetz)
+                #Plug Variables into function and store output as angles variable
+                angles = Arm.Arm3Link(q = [radians(Shoulder_Pot_Angle), radians(Elbow_Pot_Angle), 0],L = np.array([255,255,1])).inv_kin(xy = (targety, targetz))
+                print degrees(angles[1])
+                print degrees(angles[2])
+                #Find angles to set arm at
+                Shoulder_Set_Angle = (degrees(angles[0]))
+                Elbow_Set_Angle = (degrees(angles[1]))
+                #Write angles to arduino
+                arduino.write_register(1, Shoulder_Set_Angle, 1)
+                arduino.write_register(2, Elbow_Set_Angle, 1)
+                spnav_remove_events(SPNAV_EVENT_MOTION)
 except KeyboardInterrupt:
     print "Whoa there"
 
@@ -55,4 +78,5 @@ except KeyboardInterrupt:
 #=================================================================
 
 #teardown
+arduino.close()
 spnav_close() 
--- SO17___161003___Arduino_Stepper_Code/SO17___161003___Arduino_Stepper_Code.ino
+++ SO17___161003___Arduino_Stepper_Code/SO17___161003___Arduino_Stepper_Code.ino
@@ -1,8 +1,30 @@
-int en = 8;
-int xdir = 5;
-int xstp = 2;
-int ydir = 6;
-int ystp = 3;
+#include <SoftwareSerial.h>
+#include <ModbusRtu.h>
+
+// data array for modbus network sharing
+uint16_t au16data[6] = {
+  0, 0, 0, 0, 0, 0 };
+
+/**
+ *  Modbus object declaration
+ *  u8id : node id = 0 for master, = 1..247 for slave
+ *  u8serno : serial port (use 0 for Serial)
+ *  u8txenpin : 0 for RS-232 and USB-FTDI 
+ *               or any pin number > 1 for RS-485
+ */
+Modbus slave(1,0,0); // this is slave @1 and RS-232 or USB-FTDI
+
+const int en = 8;
+const int xdir = 5;
+const int xstp = 2;
+const int ydir = 6;
+const int ystp = 3;
+
+const int elbowPot = A1;
+const int shoulderPot = A0;
+
+int elbowPotVal = 0;
+int shoulderPotVal = 0;
 
 void setup() {
   pinMode(en, OUTPUT);
@@ -11,56 +33,62 @@ void setup() {
   pinMode(ydir, OUTPUT);
   pinMode(ystp, OUTPUT);
   digitalWrite(en, LOW);
-  Serial.begin(9600);
-  while (! Serial); // Wait untilSerial is ready - Leonardo
-  Serial.println("Enter LED Number 0 to 7 or 'x' to clear");
+  //Serial.begin(115200);
+  //while (! Serial); // Wait untilSerial is ready - Leonardo
+  //Serial.println("Enter LED Number 0 to 7 or 'x' to clear");
+  slave.begin( 19200 );
 }
 
 void loop() {
-  if (Serial.available()) {
-    shoulder = Serial.readStringUntil(',');
-    Serial.read();
-    elbow = Serial.readStringUntil(',');
-    Serial.print(shoulder);
-    Serial.print(elbow);
-    if ((ch >= '0') && (ch <= '6')) {
-      int axis = ch -'0';
-      if (axis == 0) {
-        digitalWrite (xdir, LOW);
-        for (int x = 40; x > 0; x--) {
-          digitalWrite(xstp, LOW);
-          delay(10);
-          digitalWrite(xstp, HIGH);
-          delay(10);
-        }
+  elbowPotVal = analogRead(elbowPot);
+  int elbowAngle = map(elbowPotVal, 0, 76, 30, 155);
+  shoulderPotVal = analogRead(shoulderPot);
+  int shoulderAngle = map(shoulderPotVal, 6, 187, 150, 0);
+    
+  au16data[2] = shoulderAngle*10;
+  au16data[3] = elbowAngle*10;
+  au16data[4] = shoulderPotVal;
+  au16data[5] = elbowPotVal;
+  
+  slave.poll( au16data, 6 );
+
+  /*if ((ch >= '0') && (ch <= '6')) {
+    int axis = ch -'0';
+    if (axis == 0) {
+      digitalWrite (xdir, LOW);
+      for (int x = 40; x > 0; x--) {
+        digitalWrite(xstp, LOW);
+        delay(10);
+        digitalWrite(xstp, HIGH);
+        delay(10);
       }
-      if (axis == 1) {
-        digitalWrite (xdir, HIGH);
-        for (int x = 40; x > 0; x--) {
-          digitalWrite(xstp, LOW);
-          delay(10);
-          digitalWrite(xstp, HIGH);
-          delay(10);
-        }
-      }
-      if (axis == 2) {
-        digitalWrite (ydir, LOW);
-        for (int x = 40; x > 0; x--) {
-          digitalWrite(ystp, LOW);
-          delay(10);
-          digitalWrite(ystp, HIGH);
-          delay(10);
-        }
-      }
-      if (axis == 3) {
-        digitalWrite (ydir, HIGH);
-        for (int x = 40; x > 0; x--) {
-          digitalWrite(ystp, LOW);
-          delay(10);
-          digitalWrite(ystp, HIGH);
-          delay(10);
-        }
+   }
+   if (axis == 1) {
+     digitalWrite (xdir, HIGH);
+     for (int x = 40; x > 0; x--) {
+       digitalWrite(xstp, LOW);
+       delay(10);
+       digitalWrite(xstp, HIGH);
+       delay(10);
+     }
+   }
+   if (axis == 2) {
+     digitalWrite (ydir, LOW);
+     for (int x = 40; x > 0; x--) {
+       digitalWrite(ystp, LOW);
+       delay(10);
+       digitalWrite(ystp, HIGH);
+       delay(10);
+     }
+   }
+   if (axis == 3) {
+     digitalWrite (ydir, HIGH);
+     for (int x = 40; x > 0; x--) {
+       digitalWrite(ystp, LOW);
+       delay(10);
+       digitalWrite(ystp, HIGH);
+       delay(10);
       }
     }
-  }
+  }*/
 }
