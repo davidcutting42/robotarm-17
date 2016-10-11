9fff7f9575cc9acc2f22e2991ad683b323ba9dab
David Cutting <davecutting2000@gmail.com>
2016-10-11 21:55:56 +0000

    updated python code

--- Rpi_Python_Code/WM_Roboarm_Inv_Kin_Controller.py
+++ Rpi_Python_Code/WM_Roboarm_Inv_Kin_Controller.py
@@ -28,20 +28,20 @@ arduino = minimalmodbus.Instrument('/dev/ttyACM0', 1) # port name, slave address
 # Create the kinematic definition object from the URDF file located in this folder.
 kin_def = ikpy.chain.Chain.from_urdf_file("WM_Roboarm_Inv_Kin_Def.URDF")
 
-#######LOOP
+while True
 
-# Inverse kinematics calculation
-target_vector = [ 0.1, -0.2, 0.1]
-target_frame = np.eye(4)
-target_frame[:3, 3] = target_vector
-print("The angles of each joints are : ", kin_def.inverse_kinematics(target_frame))
+        # Inverse kinematics calculation
+        target_vector = [ 0.1, -0.2, 0.1]
+        target_frame = np.eye(4)
+        target_frame[:3, 3] = target_vector
+        print("The angles of each joints are : ", kin_def.inverse_kinematics(target_frame))
 
-# Inverse kinematics checking
-real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_frame))
-print("Computed position vector : %s, original position vector : %s" % (real_frame[:3, 3], target_frame[:3, 3]))
+        # Inverse kinematics checking
+        real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_frame))
+        print("Computed position vector : %s, original position vector : %s" % (real_frame[:3, 3], target_frame[:3, 3]))
 
-### Plot the inverse kinematics result (uncomment the next 4 lines to enable)
-# ax = plot_utils.init_3d_figure()
-# my_chain.plot(my_chain.inverse_kinematics(target_frame), ax, target=target_vector)
-# plt.xlim(-0.1, 0.1)
-# plt.ylim(-0.1, 0.1)
+        ### Plot the inverse kinematics result (uncomment the next 4 lines to enable)
+        # ax = plot_utils.init_3d_figure()
+        # my_chain.plot(my_chain.inverse_kinematics(target_frame), ax, target=target_vector)
+        # plt.xlim(-0.1, 0.1)
+        # plt.ylim(-0.1, 0.1)
