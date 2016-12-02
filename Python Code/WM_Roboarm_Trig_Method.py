bab07406aeec8e5fb79fee97652148e61588e104
David Cutting <davecutting2000@gmail.com>
2016-12-02 16:00:51 +0000

    Added basic UI that sets values of servos over Modbus

--- Python Code/WM_Roboarm_Trig_Method.py
+++ Python Code/WM_Roboarm_Trig_Method.py
@@ -1,8 +1,23 @@
+# Arduino Modbus Register Table
+# Register:     Register Name:      Source:         Description:
+#   0               alpha               Master          Target angle for shoulder
+#   1               beta                Master          Target angle for elbow
+#   2               shoulderpot         Slave           Current angle of shoulder (debug only)
+#   3               elbowpot            Slave           Current angle of elbow (debug only)
+#   4               rawshoulder         Slave           Current analogRead value of Shoulder (debug only)
+#   5               rawelbow            Slave           Current analogRead value of Elbow (debug only)
+#   6               gamma               Master          Rotation of 3D mouse about z axis
+#   7               servo1pos           Master          Target angle of Servo A
+#   8               servo2pos           Master          Target angle of Servo B
+#   9               servo3pos           Master          Target angle of Servo C
+#   10              --                  Master          Reserved for additional servo control   
+#   11              --                  Master          Reserved for additional servo control
+
+from Tkinter import *
 import math
 import numpy as np
 from spnav import *
 import time
-from Tkinter import *
 import minimalmodbus
 
 #Find the serial port for the Arduino Mega
@@ -42,8 +57,124 @@ mouse_scal2 = 1000.0
 # Initialization of the timeout clock to zero
 elim = 0
 
-try:
-    while True:
+#Create Tkinter Window
+root = Tk()
+
+#Create servo position variables
+servo1pos=90
+servo2pos=90
+servo3pos=90
+
+#Functions for modifying servo A angle
+def servoAleft():
+    global servo1pos
+    servo1pos -= 5
+    updateServos()
+def servoAright():
+    global servo1pos
+    servo1pos += 5
+    updateServos()
+    
+#Functions for modifying servo B angle    
+def servoBleft():
+    global servo2pos
+    servo2pos -= 5
+    updateServos()
+def servoBright():
+    global servo2pos
+    servo2pos += 5
+    updateServos()
+    
+#Functions for modifying servo C angle    
+def servoCleft():
+    global servo3pos
+    servo3pos -= 5
+    updateServos()
+def servoCright():
+    global servo3pos
+    servo3pos += 5
+    updateServos()
+
+#Servo A button/label initialization and configuration
+servoalabel = Label(root, text="Servo 1")
+servoaleft = Button(root, text="<<", command=servoAleft)
+servoaangle = Label(root, text=servo1pos)
+servoaright = Button(root, text=">>", command=servoAright)
+#Servo A control object location definitions using grids.
+servoalabel.grid(row=0, column=0)
+servoaleft.grid(row=0, column=1)
+servoaangle.grid(row=0, column=2)
+servoaright.grid(row=0, column=3)
+
+#Servo B button/label initialization and configuration
+servoblabel = Label(root, text="Servo 2")
+servobleft = Button(root, text="<<", command=servoBleft)
+servobangle = Label(root, text=servo2pos)
+servobright = Button(root, text=">>", command=servoBright)
+#Servo B control object location definitions using grids.
+servoblabel.grid(row=1, column=0)
+servobleft.grid(row=1, column=1)
+servobangle.grid(row=1, column=2)
+servobright.grid(row=1, column=3)
+
+#Servo C button/label initialization and configuration
+servoclabel = Label(root, text="Servo 3")
+servocleft = Button(root, text="<<", command=servoCleft)
+servocangle = Label(root, text=servo3pos)
+servocright = Button(root, text=">>", command=servoCright)
+#Servo C control object location definitions using grids.
+servoclabel.grid(row=3, column=0)
+servocleft.grid(row=3, column=1)
+servocangle.grid(row=3, column=2)
+servocright.grid(row=3, column=3)
+
+def updateServos(): #Function for servo control
+    global servo1pos
+    global servo2pos
+    global servo3pos
+    global servoaangle
+    global servobangle
+    global servocangle
+    
+    if (servo1pos > 180):
+        servo1pos = 180
+    if (servo1pos < 0):
+        servo1pos = 0
+
+    if (servo2pos > 180):
+        servo2pos = 180
+    if (servo2pos < 0):
+        servo2pos = 0
+
+    if (servo3pos > 180):
+        servo3pos = 180
+    if (servo3pos < 0):
+        servo3pos = 0
+        
+    servoaangle.config(text=servo1pos)
+    servobangle.config(text=servo2pos)
+    servocangle.config(text=servo3pos)
+
+    arduino.write_register(7, servo1pos, 0)
+    arduino.write_register(8, servo2pos, 0)
+    arduino.write_register(9, servo3pos, 0)
+
+def updateSteppers(): #Function for stepper control and spacenav input
+
+    global H
+    global F
+    global T
+    global maxy
+    global miny
+    global minz
+    global maxz
+    global y
+    global z
+    global mouse_scal
+    global mouse_scal2
+    global elim
+    
+    try:
         #Determine if there is change in the mouse
         event = spnav_poll_event()
 
@@ -120,7 +251,11 @@ try:
         if (elim > 400):
             arduino.write_register(6, 350, 0)
             
-except KeyboardInterrupt:
-        print "Program Terminated by User"
-        
-spnav_close() #Close the spacenav channel after program ends
+                
+    except KeyboardInterrupt:
+            print "Program Terminated by User"
+    
+    root.after(1, updateSteppers)
+
+root.after(1, updateSteppers)
+root.mainloop()
