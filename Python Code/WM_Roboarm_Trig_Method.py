8a1661ae4f0a30307070c7a6518b4daaca4dd1f7
David Cutting <davecutting2000@gmail.com>
2016-12-02 04:17:48 +0000

    Final Changes before adding UI

--- Python Code/WM_Roboarm_Trig_Method.py
+++ Python Code/WM_Roboarm_Trig_Method.py
@@ -2,9 +2,10 @@ import math
 import numpy as np
 from spnav import *
 import time
-
+from Tkinter import *
 import minimalmodbus
 
+#Find the serial port for the Arduino Mega
 try:
     arduino = minimalmodbus.Instrument('/dev/ttyACM0', 1) # port name, slave address (in decimal)
 except:
@@ -13,13 +14,10 @@ except:
     except:
         arduino = minimalmodbus.Instrument('/dev/ttyACM2', 1) # port name, slave address (in decimal)
 
-
-gamma = 3.14
+#Dimensions of the Robot
 H = 240.0 # Base height in mm
 F = 255.0 # Femur length in mm
 T = 255.0 # Tibia length in mm
-y = 100
-z = H
 
 # Limit variables (mm)
 maxy = 480
@@ -27,14 +25,21 @@ miny = 130
 minz = 100
 maxz = minz + maxy
 
-# Open the 3D mouse instrunment
+# Starting Position of the Arm (mm)
+y = miny 
+z = 250
+
+# Open the 3D mouse channel
 spnav_open()
 
+# Wait 4 Seconds for all communications channels to come on board.
 time.sleep(4)
 
+#Scaling Variables for mouse to target conversion
 mouse_scal = 10.0
 mouse_scal2 = 1000.0
 
+# Initialization of the timeout clock to zero
 elim = 0
 
 try:
@@ -63,10 +68,10 @@ try:
                     y = miny
                 if (z < minz): #Constrain z
                     z = minz
-                if (y == miny):
+                if (y == miny): #Provide a case for y to prevent divide by zero
                     if (z > maxz):
                         z = maxz
-                elif ((y-miny)**2 + (z-minz)**2 > (maxy-miny)**2): #Check if outside circle
+                elif ((y-miny)**2 + (z-minz)**2 > (maxy-miny)**2): #Check if outside the circle that we want to constrain it in
                     m = (z - minz) / (y - miny)
                     d = z - m * y
                     r = maxy - miny
@@ -76,38 +81,46 @@ try:
                     print "hello"
 
                     
-                L = math.sqrt(math.pow(y,2) + math.pow(H-z,2))
-                
+                L = math.sqrt(math.pow(y,2) + math.pow(H-z,2)) #Calculate the distance from the base to the end of the arm
+
+                # Two different cases to make sure math is correct for when the end is above and below the base.
                 if (z > H):
                     alpha1 = math.acos(y/L) + math.pi/2.0
                 else:
                     alpha1 = math.asin(y/L)
 
-                    
+                # Calculate angles for potentiometer targets in joint space.
                 beta = math.acos((math.pow(L,2) - math.pow(T,2) - math.pow(F,2))/(-2.0*T*F))
                 alpha2 = math.acos((math.pow(T,2) - math.pow(F,2) - math.pow(L,2))/(-2.0*F*L))     
                 alpha = alpha1 + alpha2
 
+                # Get potentiometer data from the arduino
                 shoulderpot = arduino.read_register(2, 1)
                 elbowpot = arduino.read_register(3, 1)
                 rawshoulder = arduino.read_register(4, 0)
                 rawelbow = arduino.read_register(5, 0)
-                
+
+                # Print targets, current positions, etc.
                 print "alph: {:.2f}, beta: {:.2f}, y: {:.2f}, z: {:.2f}, shoulder: {}, elbow: {} rawshoulder: {} rawelbow: {} gamma: {}".format(np.degrees(alpha), np.degrees(beta), y, z, shoulderpot, elbowpot, rawshoulder, rawelbow, gamma)
 
+                # Write joint space targets to arduino
                 arduino.write_register(0, int(math.degrees(alpha)*10), 0)
                 arduino.write_register(1, int(math.degrees(beta)*10), 0)
                 arduino.write_register(6, gamma, 0)
 
+                # Remove extra events to avoid lag in spacenav.
                 spnav_remove_events(SPNAV_EVENT_MOTION)
 
+                #Reset the timeout variable
                 elim = 0;
+
+        #Check to see if timeout has occoured, then set the mouse inputs to zero so drift is eliminated.
         if (event == None):
             elim = elim + 1
         if (elim > 400):
             arduino.write_register(6, 350, 0)
+            
 except KeyboardInterrupt:
-        print "Whoa there"
+        print "Program Terminated by User"
         
-spnav_close() 
-
+spnav_close() #Close the spacenav channel after program ends
