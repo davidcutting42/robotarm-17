03dc4620aa1d0da5fec4f5de9b788a71e900c766
David Cutting <davecutting2000@gmail.com>
2017-01-03 01:16:25 +0000

    Bug fixes and stability upgrades. There's something still wrong with the atan2 function that calculates the beta angle, needs more work.

--- MK2-WM-Roboarm/Arduino_Code_MK2_WM_Roboarm/Arduino_Code_MK2_WM_Roboarm.ino
+++ MK2-WM-Roboarm/Arduino_Code_MK2_WM_Roboarm/Arduino_Code_MK2_WM_Roboarm.ino
@@ -1,5 +1,5 @@
 #include "MyTypes.h"
-#define stpmode 16 // Sets the stepping mode of all 3 motors. Set to 1, 2, 4, 8, 16, or 32
+#define stpmode 8 // Sets the stepping mode of all 3 motors. Set to 1, 2, 4, 8, 16, or 32
 
 // Set pin numbers for steppers A, B, and C
 const int astep = 4;
@@ -21,22 +21,24 @@ const int yoffset = 27;
 const int startbutton = A2;
 
 // Set lengths of humerus and ulna
-float h = 249.2;
-float u = 249.2;
+double h = 249.2;
+double u = 249.2;
 
 
 
 // Create arrays used to store the position of each predefined waypoint {xcoord, ycoord, leftorrightbend, baseangle}
-waypoint homepos = {0, h + u, 0, 90}; 
+waypoint homepos = {0, h + u, 1, 90}; 
 waypoint stack1 = {-280, -213, 0, 0};
 waypoint stack2 = {85, 68, 1, 0};
 waypoint stack3 = {295, 277, 1, 0};
-waypoint stack4 = {85, 487, 1, 0};
+waypoint stack4 = {85, 487, 0, 0};
 waypoint stack5 = {-124, 277, 0, 0};
 waypoint targetcenter = {85, 277, 1, 0};
 
+waypoint wp[7] = {homepos, stack1, stack2, stack3, stack4, stack5, targetcenter};
+
 // Array for storing calculated angles
-float angles[2];
+double angles[2];
 
 // Number of loops through main loop
 int k = 1;
@@ -53,9 +55,9 @@ int astepdirection = 0;
 int bstepdirection = 0;
 int cstepdirection = 0;
 
-const unsigned long motadelay = 5000;
-const unsigned long motbdelay = 1000;
-const unsigned long motcdelay = 500;
+const unsigned long motadelay = 5000 / stpmode;
+const unsigned long motbdelay = 5000 / stpmode;
+const unsigned long motcdelay = 1000 / stpmode;
 
 int astepswitch = 0;
 int bstepswitch = 0;
@@ -69,9 +71,11 @@ int effectorpositionstatus = 0;
 int runonstarta = 1;
 
 // Set ratio for number of steps to number of degrees
-const float aratio = 200.0 * stpmode / 360.0;
-const float bratio = 200.0 * 3.0 / 2.0 * stpmode / 360.0;
-const float cratio = 200.0 * 50.0 * stpmode / 360.0;
+const double aratio = 200.0 * 32 / 10 * stpmode / 360.0;
+const double bratio = 200.0 * 24 / 10 * stpmode / 360.0;
+const double cratio = 200.0 * 50.0 * stpmode / 360.0;
+
+int waypointselect = 0;
 
 void setup() {
   // Set each control pin to an output
@@ -139,57 +143,21 @@ void setup() {
   //bstepdifference=50*stpmode*3/2;
   //cstepdifference=50*stpmode*50;
   
-  inversekinematics(stack3);
+  
 }
 
 void loop() {
-  /*
-  switch (effectorpositionstatus) {
-    case 0:
-      while (runonstarta) {
-        inversekinematics(homepos);
-        runonstarta = 0;
-      }
-      if (astepdifference == 0 && bstepdifference == 0 && cstepdifference == 0) {
-        effectorpositionstatus = 1;
-      }
-      break;
-    case 1:
-      inversekinematics(stack1);
-      if (astepdifference == 0 && bstepdifference == 0 && cstepdifference == 0) {
-        effectorpositionstatus = 2;
-      }
-      break;
-    case 2:
-      inversekinematics(stack2);
-      if (astepdifference == 0 && bstepdifference == 0 && cstepdifference == 0) {
-        effectorpositionstatus = 3;
-      }
-      break;
-    case 3:
-      inversekinematics(stack3);
-      if (astepdifference == 0 && bstepdifference == 0 && cstepdifference == 0) {
-        effectorpositionstatus = 4;
-      }
-      break;
-    case 4:
-      inversekinematics(stack4);
-      if (astepdifference == 0 && bstepdifference == 0 && cstepdifference == 0) {
-        effectorpositionstatus = 5;
-      }
-      break;
-    case 5:
-      inversekinematics(stack5);
-      if (astepdifference == 0 && bstepdifference == 0 && cstepdifference == 0) {
-        effectorpositionstatus = 0;
-      }
-      break;
-      
+  long stepdifferenceall = astepdifference + bstepdifference + cstepdifference;
+ 
+  if (stepdifferenceall == 0 && waypointselect < 7) {
+    inversekinematics(wp[waypointselect]);
+    waypointselect++;
   }
-  */
+    
+  
   switch (astepswitch) {
     case 0:
-      if (astepdifference != 0) {
+      if ((astepdifference != 0) && (cstepdifference == 0)) {
         astepswitch = 1;
         astepswitchtimer = micros();
       }
@@ -207,6 +175,7 @@ void loop() {
         astepswitchtimer = micros();
         if(astepdirection == 0) {
           astepcount++;
+          
         }
         else if(astepdirection == 1) {
           astepcount--;
@@ -225,7 +194,7 @@ void loop() {
 
   switch (bstepswitch) {
     case 0:
-      if (bstepdifference != 0) {
+      if ((bstepdifference != 0) && (cstepdifference == 0)) {
         bstepswitch = 1;
         bstepswitchtimer = micros();
       }
@@ -242,12 +211,16 @@ void loop() {
         digitalWrite(bstep, HIGH);
         bstepswitchtimer = micros();
         if(bstepdirection == 0) {
-          bstepcount--;
-        }
-        else if(bstepdirection == 1) {
           bstepcount++;
         }
+        else if(bstepdirection == 1) {
+          bstepcount--;
+        }
         bstepdifference--;
+        //Serial.print("bstepdifference = ");
+        //Serial.print(bstepdifference);
+        //Serial.print("bstepcount = ");
+        //Serial.println(bstepcount);
         if(bstepdifference == 0) {
           bstepswitch = 0;
         }
@@ -273,7 +246,7 @@ void loop() {
       }
       break;
     case 2:
-      if(micros() >= (cstepswitchtimer >= motcdelay)) {
+      if(micros() >= (cstepswitchtimer + motcdelay)) {
         digitalWrite(cstep, HIGH);
         cstepswitchtimer = micros();
         if(cstepdirection == 0) {
@@ -302,45 +275,56 @@ void loop() {
 
 void inversekinematics(waypoint target) {
   //Calculating the two different possibilities for the beta angle
-  float beta1 = atan2(sqrt(1-((pow(target.x, 2)+pow(target.y, 2)-pow(h, 2)-pow(u, 2))/(2 * h * u))),(pow(target.x, 2)+pow(target.y, 2)-pow(h, 2)-pow(u, 2))/(2 * h * u));
-  float beta2 = atan2(-sqrt(1-((pow(target.x, 2)+pow(target.y, 2)-pow(h, 2)-pow(u, 2))/(2 * h * u))),(pow(target.x, 2)+pow(target.y, 2)-pow(h, 2)-pow(u, 2))/(2 * h * u));
+  double beta1 = atan2((sqrt(1-((pow(target.x, 2)+pow(target.y, 2)-pow(h, 2)-pow(u, 2))/(2.0 * h * u)))),((pow(target.x, 2)+pow(target.y, 2)-pow(h, 2)-pow(u, 2))/(2.0 * h * u)));
+  double beta2 = atan2((-(sqrt(1-((pow(target.x, 2)+pow(target.y, 2)-pow(h, 2)-pow(u, 2))/(2.0 * h * u))))),((pow(target.x, 2)+pow(target.y, 2)-pow(h, 2)-pow(u, 2))/(2.0 * h * u)));
+  
+  Serial.print("beta1 = ");
+  Serial.println(beta1);
+  Serial.print("beta2 = ");
+  Serial.println(beta2);
 
   // Calculating the two different possibilities for the alpha angle
-  float k2a = u * sin(beta1);
-  float k2b = u * sin(beta2);
-  float k1a = h + (u * cos(beta1));
-  float k1b = h + (u * cos(beta2));
-  float alpha1 = atan2(target.y, target.x)-atan2(k2a, k1a);
-  float alpha2 = atan2(target.y, target.x)-atan2(k2b, k1b);
+  double k2a = u * sin(beta1);
+  double k2b = u * sin(beta2);
+  double k1a = h + (u * cos(beta1));
+  double k1b = h + (u * cos(beta2));
+  double alpha1 = atan2(target.y, target.x)-atan2(k2a, k1a);
+  double alpha2 = atan2(target.y, target.x)-atan2(k2b, k1b);
 
   // Create an array of the 4 calculated alpha and beta angle, converted to degrees.
-  float trigresults[] = {alpha1*180/3.14159, alpha2*180/3.14159, beta1*180/3.14159, beta2*180/3.14159};
+  double trigresults[] = {alpha1*180.0/3.14159, alpha2*180.0/3.14159, beta1*180.0/3.14159, beta2*180.0/3.14159};
 
   // Make all negative angles positive
-  if(trigresults[0] < 0) {
+  while(trigresults[0] < 0) {
     trigresults[0] += 360;
   }
-  if(trigresults[1] < 0) {
+  while(trigresults[1] < 0) {
     trigresults[1] += 360;
   }
-  if(trigresults[2] < 0) {
+  while(trigresults[2] < 0) {
     trigresults[2] += 360;
   }
-  if(trigresults[3] < 0) {
+  while(trigresults[3] < 0) {
     trigresults[3] += 360;
   }
+  
+  Serial.println("NEWWWWWWW");
+  Serial.println(trigresults[0]);
+  Serial.println(trigresults[1]);
+  Serial.println(trigresults[2]);
+  Serial.println(trigresults[3]);
 
   // Variables for check that ensures angles are within permissible range
   boolean checkone = true;
   boolean checktwo = true;
 
   // Test alpha1 and beta1 to see if they are inside the allowable limits
-  if((trigresults[0] > 200 && trigresults[0] < 360) || (trigresults[2] > 160 && trigresults[2] < 340) || (trigresults[0] != trigresults[0]) || (trigresults[2] != trigresults[2])) {
+  if((trigresults[0] > 200 && trigresults[0] < 360) || (trigresults[2] > 160 && trigresults[2] < 185) || (trigresults[0] != trigresults[0]) || (trigresults[2] != trigresults[2])) {
     checkone = false;
   }
   
   // Test alpha2 and beta2 to see if they are inside the allowable limits
-  if((trigresults[1] > 200 && trigresults[1] < 360) || (trigresults[3] > 160 && trigresults[3] < 340) || (trigresults[1] != trigresults[1]) || (trigresults[3] != trigresults[3])) {
+  if((trigresults[1] > 200 && trigresults[1] < 360) || (trigresults[3] > 160 && trigresults[3] < 18) || (trigresults[1] != trigresults[1]) || (trigresults[3] != trigresults[3])) {
     checktwo = false;
   }
 
@@ -392,61 +376,65 @@ void inversekinematics(waypoint target) {
     astepdifference = 0;  
   }
 
-  if(stepperBtarget < 100 * stpmode && bstepcount > 100 * stpmode) {
+  if((stepperBtarget < bdegreesstep(180)) && (bstepcount > bdegreesstep(180))) {
     digitalWrite(bdir, LOW);
-    bstepdirection = 1;
-    bstepdifference = 200 * stpmode - bstepcount + stepperBtarget;
-  }
-  else if(stepperBtarget > 100 * stpmode && bstepcount < 100 * stpmode) {
-    digitalWrite(bdir, HIGH);
     bstepdirection = 0;
-    bstepdifference = 200 * stpmode - stepperBtarget + bstepcount;
+    bstepdifference = abs(bdegreesstep(360) - bstepcount + stepperBtarget);
+  }
+  else if((stepperBtarget > bdegreesstep(180)) && (bstepcount < bdegreesstep(180))) {
+    digitalWrite(bdir, HIGH);
+    bstepdirection = 1;
+    bstepdifference = abs(bdegreesstep(360) - stepperBtarget + bstepcount);
   }
   else if(stepperBtarget > bstepcount) {
     digitalWrite(bdir, LOW);
-    bstepdirection = 1;
-    bstepdifference = stepperBtarget - bstepcount;
+    bstepdirection = 0;
+    bstepdifference = abs(stepperBtarget - bstepcount);
   }
   else if(stepperBtarget < bstepcount) {
     digitalWrite(bdir, HIGH);
-    bstepdirection = 0;
-    bstepdifference = bstepcount - stepperBtarget;
+    bstepdirection = 1;
+    bstepdifference = abs(bstepcount - stepperBtarget);
   }
   else {
     bstepdifference = 0;  
   }
 
   if(stepperCtarget < cstepcount) {
-    cstepdifference = cstepcount - stepperCtarget;
-    digitalWrite(cdir, HIGH); 
+    digitalWrite(cdir, HIGH);
+    cstepdirection = 0;
+    cstepdifference = cstepcount - stepperCtarget; 
+  }
+  else if(stepperCtarget > cstepcount) {
+    digitalWrite(cdir, LOW);
+    cstepdirection = 1;
+    cstepdifference = stepperCtarget - cstepcount;
   }
   else {
-    cstepdifference = stepperCtarget - cstepcount;
-    digitalWrite(cdir, LOW); 
+    cstepdifference = 0;  
   }
-  Serial.println(cstepdifference);
 }
 
-float astepdegrees(long steps) {
-  return (float)steps / aratio;
+double astepdegrees(long steps) {
+  return (double)steps / aratio;
 }
 
-float bstepdegrees(long steps) {
-  return (float)steps / bratio;
+double bstepdegrees(long steps) {
+  return (double)steps / bratio;
 }
 
-float cstepdegrees(long steps) {
-  return (float)steps / cratio;
+double cstepdegrees(long steps) {
+  return (double)steps / cratio;
 }
 
-long adegreesstep(float deg) {
+long adegreesstep(double deg) {
   return (long)(deg * aratio);
 }
 
-long bdegreesstep(float deg) {
+long bdegreesstep(double deg) {
   return (long)(deg * bratio);
 }
 
-long cdegreesstep(float deg) {
+long cdegreesstep(double deg) {
   return (long)(deg * cratio);
 }
